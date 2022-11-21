a3822c40707585892e500fdfd3b66d529d5f4817
Egemenatilla <44278787+Egemenatilla@users.noreply.github.com>
2022-11-22 01:47:45 +0300

    Add files via upload

---
+++ Java101/BüyüktenKüçüğe/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int s1,s2,s3,ebs=0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Sırayla Sayıları Girin: ");
+        s1 = input.nextInt();
+        s2 = input.nextInt();
+        s3 = input.nextInt();
+
+        if ((s1 > s2) && (s1 > s3))
+        {
+            if (s2>s3)
+                System.out.println(s1+" > "+s2+" > "+s3);
+            else
+                System.out.println(s1+" > "+s3+" > "+s2);
+
+        }
+        else if ((s2>s1) && (s2>s3))
+        {
+            if (s1>s3)
+                System.out.println(s2+" > "+s1+" > "+s3);
+            else
+                System.out.println(s2+" > "+s3+" > "+s1);
+        }
+        else
+        {
+            if (s1>s2)
+                System.out.println(s3+" > "+s1+" > "+s2);
+            else
+                System.out.println(s3+" > "+s2+" > "+s1);
+        }
+
+
+    }
+}
