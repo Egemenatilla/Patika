6d375224b654fe23e34a2882e171927d4b175904
Egemenatilla <44278787+Egemenatilla@users.noreply.github.com>
2022-11-21 23:14:51 +0300

    Add files via upload

---
+++ Java101/Etkinlik Önerme/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int heat;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklık giriniz : ");
+        heat = input.nextInt();
+
+        if(heat < 5 )
+        {
+            System.out.println("Kayak yapabilirsiniz.");
+        }
+
+        else if(heat >= 5 && heat <= 15)
+        {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        }
+
+        else if(heat >15 && heat <= 25)
+        {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        }
+
+        else if (heat > 25)
+        {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+
+        else
+        {
+            System.out.println("Hatalı giriş!");
+        }
+    }
+}
